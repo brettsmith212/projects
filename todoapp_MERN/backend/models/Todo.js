94d32895e5982fbeee07fe854b385e4ec8fe917b
Brett Smith <brettsmith212@gmail.com>
2021-09-13 15:23:22 -0700

    added folder

--- todoapp_MERN/backend/models/Todo.js
+++ todoapp_MERN/backend/models/Todo.js
@@ -3,3 +3,5 @@ const mongoose = require("mongoose");
 const TodoSchema = new mongoose.Schema({title: String, completed: Boolean})
 
 module.exports = mongoose.model("Todo", TodoSchema);
+
+// comment
