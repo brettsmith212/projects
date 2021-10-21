142f1be477eaa070ef32fa1e4b877fd4ee95f50c
Brett Smith <brettsmith212@gmail.com>
2021-10-21 15:45:06 -0700

    add screenshots to loggedOut.js

--- doordashlog/src/components/loggedOut/LoggedOut.js
+++ doordashlog/src/components/loggedOut/LoggedOut.js
@@ -6,10 +6,34 @@ function LoggedOut() {
   return (
     <main className="main">
       <div className="hero">
-        <h1 className="title">Welcome To Delivery Log!</h1>
+        <h1 className="title">Welcome to Delivery Log</h1>
+        <h2 className="subtitle">
+          Track your earnings in a simple and modern dashboard
+        </h2>
         <img src={waves} alt="waves" className="waves" />
       </div>
-      <div className="features"></div>
+      <div className="features">
+        <h2 className="features-header">
+          A delivery dashboard for everyone...
+        </h2>
+        <div className="features-images">
+          <img
+            src={require("../images/home.png")}
+            alt="home screenshot"
+            className="home-screenshot"
+          />
+          <img
+            src={require("../images/analytics.png")}
+            alt="analytics screenshot"
+            className="analytics-screenshot"
+          />
+          <img
+            src={require("../images/table.png")}
+            alt="table screenshot"
+            className="table-screenshot"
+          />
+        </div>
+      </div>
     </main>
   );
 }
