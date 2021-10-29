3d024832e43c3a9e8c28c884c3eed8b63a4f7bc7
Brett Smith <brettsmith212@gmail.com>
2021-10-28 22:53:04 -0700

    refactored loggedOut CSS and ready for desktop view

--- doordashlog/src/components/loggedOut/LoggedOut.js
+++ doordashlog/src/components/loggedOut/LoggedOut.js
@@ -6,35 +6,22 @@ function LoggedOut() {
   return (
     <main className="main">
       <div className="hero">
-        <h1 className="title">Welcome to Delivery Log</h1>
-        <h2 className="subtitle">
-          Track your earnings in a simple and modern dashboard
-        </h2>
-        <img src={waves} alt="waves" className="waves" />
+        <h1>Welcome to Delivery Log</h1>
+        <h2>Track your earnings in a simple and modern dashboard</h2>
       </div>
-      <div className="features">
-        <h2 className="features-header">
-          An easier way to monitor and calculate your earnings
-        </h2>
-        <div className="features-images">
-          <video className="tutorial-video" autoPlay loop>
+      <a href="#features">Learn More</a>
+      <div id="features">
+        <div className="showcase">
+          <h2>An easier way to monitor and calculate your earnings</h2>
+          <video autoPlay loop>
             <source src={require("../images/tutorial.mov")} type="video/mp4" />
           </video>
-          <img
-            src={require("../images/home.png")}
-            alt="home screenshot"
-            className="home-screenshot"
-          />
+          <img src={require("../images/home.png")} alt="home screenshot" />
           <img
             src={require("../images/analytics.png")}
             alt="analytics screenshot"
-            className="analytics-screenshot"
           />
-          <img
-            src={require("../images/table.png")}
-            alt="table screenshot"
-            className="table-screenshot"
-          />
+          <img src={require("../images/table.png")} alt="table screenshot" />
         </div>
       </div>
     </main>
